425f68970501b971850f0c1573731fe1f0f43370
Emma Craig <emma.ryal@gmail.com>
2021-02-18 18:23:02 +0100

    cart saved to checkout

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,25 @@ const express = require('express')
 const router = express.Router()
 
 
+//middlewares
+const { authCheck} = require("../middlewares/auth");
 
-router.get('/user', (req, res)  => {
-    //express app is a request response handler
-    res.json({
-        data: 'hey you hit user API end point',
-    });
-});
+//controllers
+const {userCart} = require('./../controllers/user');
+const {getUserCart} = require('./../controllers/user');
+const {emptyUserCart} = require('./../controllers/user');
+const {saveAddress} = require('./../controllers/user');
+
+
+router.post('/user/cart', authCheck, userCart); //save Cart
+router.get ('/user/cart', authCheck, getUserCart); //get cart
+router.delete('/user/cart', authCheck, emptyUserCart)
+router.post('/user/address', authCheck, saveAddress)
+// router.get('/user', (req, res)  => {
+//     //express app is a request response handler
+//     res.json({
+//         data: 'hey you hit user API end point',
+//     });
+// });
 
 module.exports = router
