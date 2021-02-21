bc9a3305f518d34fe97f747ae88c376b6804f173
Emma Craig <emma.ryal@gmail.com>
2021-02-21 10:49:09 +0100

    stripe and save orders

--- routes/user.js
+++ routes/user.js
@@ -7,16 +7,19 @@ const router = express.Router()
 const { authCheck} = require("../middlewares/auth");
 
 //controllers
-const {userCart} = require('./../controllers/user');
-const {getUserCart} = require('./../controllers/user');
-const {emptyUserCart} = require('./../controllers/user');
-const {saveAddress} = require('./../controllers/user');
+
+const {userCart, getUserCart, emptyUserCart, saveAddress, createOrder, orders} = require('./../controllers/user');
 
 
 router.post('/user/cart', authCheck, userCart); //save Cart
 router.get ('/user/cart', authCheck, getUserCart); //get cart
 router.delete('/user/cart', authCheck, emptyUserCart)
 router.post('/user/address', authCheck, saveAddress)
+
+router.post('/user/order', authCheck, createOrder)
+router.get('/user/orders', authCheck, orders)
+
+
 // router.get('/user', (req, res)  => {
 //     //express app is a request response handler
 //     res.json({
