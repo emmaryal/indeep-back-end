c021a46a755945231f4c210c6236fa1239114aa2
Emma Craig <emma.ryal@gmail.com>
2021-02-24 18:54:09 +0100

    edit config vars

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,21 +5,18 @@ const Cart = require("./../models/cart");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
-    
+  const user = await User.findOne({ email: req.user.email }).exec();
 
-    const user = await User.findOne({email: req.user.email}).exec()
+  const { cartTotal } = await Cart.findOne({ orderedBy: user._id }).exec();
+  console.log("CART TOTAL CHARGED", cartTotal);
 
-    const {cartTotal} = await Cart.findOne({orderedBy: user._id}).exec()
-    console.log('CART TOTAL CHARGED', cartTotal)
-  
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: cartTotal *100,
-      currency: "eur",
-    });
-    console.log("PAYMENT INTENT, controller stripe", paymentIntent)
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: cartTotal * 100,
+    currency: "eur",
+  });
+  console.log("PAYMENT INTENT, controller stripe", paymentIntent);
 
-  
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  };
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};
--- server.js
+++ server.js
@@ -1,69 +1,59 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const {readdirSync} = require('fs');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { readdirSync } = require("fs");
 
 // const dotenv = require("dotenv");
 
 // dotenv.config();
-require('dotenv').config()
+require("dotenv").config();
 
 //app
-const app = express()
+const app = express();
 
 //db
 //mongoose.connect(process.env.MONGO_URI, {
-mongoose.connect(process.env.DATABASE, {
-useNewUrlParser: true,
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
-})
-.then(()=> console.log('DB CONNECTED'))
-.catch(err => console.log(`DB CONNECTION ERR : ${err}`))
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log(`DB CONNECTION ERR : ${err}`));
 
 //CORS SETTINGS
 app.use(
-    cors({
-        credentials: true,
-        origin: [
-            "http://localhost:3000",
-            "https://indeeprecords.herokuapp.com",
-            "http://indeeprecords.herokuapp.com"
-        ]
-    })
-)
-
-
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "https://indeeprecords.herokuapp.com",
+      "http://indeeprecords.herokuapp.com",
+    ],
+  })
+);
 
 //middlewares
 
-app.use(morgan('dev'));
-app.use(bodyParser.json({limit: '2mb'}));
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-
-
-
 //routes middleware
-readdirSync('./routes').map((r)=>
-app.use('/api', require('./routes/' + r)));
-
-
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 //route for serving react app(index.html)
 // app.use((req, res, next) => {
 //     res.sendFile(__dirname + "/public/index.html")
 // });
 
-
-app.use(express.static('public'))
-
+// app.use(express.static("public"));
 
 //port
 const port = process.env.PORT || 8000;
 
-app.listen(port, ()=> console.log(`Server is running on port${port}`))
-
+app.listen(port, () => console.log(`Server is running on port${port}`));
