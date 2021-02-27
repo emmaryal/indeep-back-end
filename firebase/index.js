8f541a5a2fc50b00b841f99d666c66bc9dcbad42
Emma Craig <emma.ryal@gmail.com>
2021-02-27 20:09:27 +0100

    fb vars

--- firebase/index.js
+++ firebase/index.js
@@ -3,7 +3,7 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL:
     "https://indeep-3ec84-default-rtdb.europe-west1.firebasedatabase.app",
 });
