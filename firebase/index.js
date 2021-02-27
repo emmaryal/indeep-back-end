1d2472119527105f26cca30e09f9b845728dc4a1
Emma Craig <emma.ryal@gmail.com>
2021-02-27 19:08:30 +0100

    fb key to dotenv

--- firebase/index.js
+++ firebase/index.js
@@ -3,9 +3,16 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(process.env.FB_SERVICE_ACCOUNT_KEY),
   databaseURL:
     "https://indeep-3ec84-default-rtdb.europe-west1.firebasedatabase.app",
 });
+
+
+
+
+
+
 
 module.exports = admin;
