57b452b45c9f75fd5450d8b03b71b50dfaff3b79
Emma Craig <emma.ryal@gmail.com>
2021-02-27 18:49:06 +0100

    deploy

--- firebase/index.js
+++ firebase/index.js
@@ -1,11 +1,11 @@
-
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://indeep-3ec84-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL:
+    "https://indeep-3ec84-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
 module.exports = admin;
